906e862cbc9fa3b88d988ff0f1c586e79c68b3df
dimastranggono12@gmail.com <dimastranggono12@gmail.com>
2021-02-25 08:26:29 +0700

    TUGAS

---
+++ WEEK2/jackCard/JackCard/Assets/ArrangeActions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrangeActions : MonoBehaviour
+{
+
+    private RectTransform panelRectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelRectTransform = GetComponent<RectTransform>();
+    }
+
+    public void MoveDownOne()
+    {
+        print("(before change)" + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+        int currentSiblingIndex =
+            panelRectTransform.GetSiblingIndex();
+            panelRectTransform.SetSiblingIndex(currentSiblingIndex - 1);
+        print("(after change)" + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+    }
+
+    public void MoveUpOne()
+    {
+        print("(before change)" + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+        int currentSiblingIndex =
+            panelRectTransform.GetSiblingIndex();
+        panelRectTransform.SetSiblingIndex(currentSiblingIndex + 1);
+        print("(after change)" + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+    }
+
+}
