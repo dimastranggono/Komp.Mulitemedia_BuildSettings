16d4f7a94589af7d28417ffd1573062c86a9a7a2
dimastranggono12@gmail.com <dimastranggono12@gmail.com>
2021-03-09 16:13:27 +0700

    week4 and 3

---
+++ Week3Tugas/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    public Text humanText;
+    private int totalHuman = 0;
+    void Start()
+    {
+        UpdateHumanText();
+    }
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Human"))
+        {
+            totalHuman++;
+            UpdateHumanText();
+            Destroy(hit.gameObject);
+        }
+    }
+    private void UpdateHumanText()
+    {
+        string starMessage = "Humans = " + totalHuman;
+        humanText.text = starMessage;
+    }
+}
